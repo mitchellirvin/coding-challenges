d6c0dc8b1eb24bc05737881aa6c7f73194879c01
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2018-03-13 12:47:26 -0400

    added program that modifies first half of linked list as a function of the second half of the linked list

---
+++ data-structures/linked-lists/SubtractFromFirstHalf.java
@@ -0,0 +1,61 @@
+// https://www.interviewbit.com/problems/subtract/
+
+import java.util.*;
+
+public class SubtractFromFirstHalf {
+
+    static class ListNode {
+       public int val;
+       public ListNode next;
+       ListNode(int x) { val = x; next = null; }
+    }
+
+    public static ListNode subtract(ListNode A) {
+        ListNode head = A;
+        ArrayDeque<Integer> secondHalfOfList = getSecondHalfAsStack(A);
+
+        while(!secondHalfOfList.isEmpty()) {
+            A.val = secondHalfOfList.pop() - A.val;
+            A = A.next;
+        }
+
+        return head;
+    }
+
+    public static ArrayDeque<Integer> getSecondHalfAsStack(ListNode head) {
+        ArrayDeque<Integer> secondHalfOfList = new ArrayDeque<>();
+        ListNode iter = head;
+
+        int numOfNodes = 0;
+        while(iter != null) {
+            iter = iter.next;
+            numOfNodes++;
+        }
+
+        iter = head;
+        int midPoint = numOfNodes / 2;
+        if(numOfNodes % 2 == 1) ++midPoint;
+
+        for(int i = 0; i < numOfNodes; i++) {
+            if(i >= midPoint) {
+                secondHalfOfList.push(iter.val);
+            }
+            iter = iter.next;
+        }
+
+        return secondHalfOfList;
+    }
+
+    public static void main(String[] args) {
+      ListNode head = new ListNode(5);
+      head.next = new ListNode(9);
+      head.next.next = new ListNode(7);
+      System.out.println("Should print 2 -> 9 -> 7");
+      ListNode result = subtract(head);
+      while(result != null) {
+        System.out.print(result.val + " -> ");
+        result = result.next;
+      }
+      System.out.println();
+    }
+}
