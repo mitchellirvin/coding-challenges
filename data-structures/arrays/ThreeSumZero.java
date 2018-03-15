4014753f1e868f0cd01937efc18bb11bc2f39843
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2018-03-14 21:25:08 -0400

    three sum zero solved w/ two pointer strategy

--- data-structures/arrays/ThreeSumZero.java
+++ data-structures/arrays/ThreeSumZero.java
@@ -2,54 +2,67 @@
 
 import java.util.*;
 
-public class Solution {
-    public static class SumPair {
-        int sum;
-        int firstVal;
-        int firstIndex;
-        int secondVal;
-        int secondIndex;
-        public SumPair(int sum, int firstVal, int firstIndex, int secondVal, int secondIndex) {
-            this.sum = sum;
-            this.firstVal = firstVal;
-            this.firstIndex = firstIndex;
-            this.secondVal = secondVal;
-            this.secondIndex = secondIndex;
-        }
-    }
+public class ThreeSumZero {
 
     public static ArrayList<ArrayList<Integer>> threeSum(ArrayList<Integer> A) {
         ArrayList<ArrayList<Integer>> triplets = new ArrayList<>();
 
         Collections.sort(A);
-        ArrayList<SumPair> pairSums = new ArrayList<>();
         for(int i = 0; i < A.size(); i++) {
-            for(int j = i + 1; j < A.size(); j++) {
-                pairSums.add(new SumPair(A.get(i) + A.get(j), A.get(i), i, A.get(j), j));
-            }
-        }
-
-        for(int i = 0; i < A.size(); i++) {
-            for(int j = 0; j < pairSums.size(); j++) {
-                if((A.get(i) + pairSums.get(j).sum == 0) &&
-                    (i != pairSums.get(j).firstIndex && i != pairSums.get(j).secondIndex)) {
-                        ArrayList<Integer> triplet = new ArrayList<>();
-                        triplet.add(A.get(i));
-                        triplet.add(pairSums.get(j).firstVal);
-                        triplet.add(pairSums.get(j).secondVal);
-                        Collections.sort(triplet);
-                        if(!triplets.contains(triplet)) {
-                            triplets.add(triplet);
-                        }
-                    }
+            int k = A.size() - 1;
+            int j = i + 1;
+            while(k > j) {
+                if((-A.get(i)) == (A.get(j) + A.get(k))) {
+                    ArrayList<Integer> triplet = new ArrayList<>();
+                    // System.out.print(A.get(i) + " " + A.get(j) + " " + A.get(k) + " | ");
+                    triplet.add(A.get(i));
+                    triplet.add(A.get(j));
+                    triplet.add(A.get(k));
+                    Collections.sort(triplet);
+                    if(!triplets.contains(triplet)) triplets.add(triplet);
+                    j++;
+                    k--;
+                } else if((-A.get(i)) > (A.get(j) + A.get(k))) j++;
+                else k--;
             }
         }
 
         return triplets;
     }
 
+
     public static void main(String[] args) {
       // call three sum.
-      // currently fails the interviewbit test because of inefficiency
+      ArrayList<Integer> test = new ArrayList<>();
+
+      test.add(1);
+      test.add(-4);
+      test.add(0);
+      test.add(0);
+      test.add(5);
+      test.add(-5);
+      test.add(1);
+      test.add(0);
+      test.add(-2);
+      test.add(4);
+      test.add(-4);
+      test.add(1);
+      test.add(-1);
+      test.add(-4);
+      test.add(3);
+      test.add(4);
+      test.add(-1);
+      test.add(-1);
+      test.add(-3);
+
+      // expected [-5 0 5 ] [-5 1 4 ] [-4 -1 5 ] [-4 0 4 ] [-4 1 3 ] [-3 -2 5 ]
+      // [-3 -1 4 ] [-3 0 3 ] [-2 -1 3 ] [-2 1 1 ] [-1 0 1 ] [0 0 0 ]
+      ArrayList<ArrayList<Integer>> results = threeSum(test);
+      for(ArrayList<Integer> list : results) {
+        for(int n : list) {
+          System.out.print(n + " ");
+        }
+        System.out.println();
+      }
     }
 }
