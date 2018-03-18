d8fb6a7eb4038e08f261829bed316854330dd74d
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2018-03-17 22:25:55 -0400

    adding links to challenges for trie and heap from ctci

--- data-structures/heaps-and-maps/RunningMedian.java
+++ data-structures/heaps-and-maps/RunningMedian.java
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/ctci-find-the-running-median/problem
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
--- data-structures/trees/Trie.java
+++ data-structures/trees/Trie.java
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/ctci-contacts/problem
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
