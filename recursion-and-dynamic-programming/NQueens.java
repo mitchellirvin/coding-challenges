c1075625f66dd7ab2f8e125e86aa7a7fc6b3b68b
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2018-07-19 13:54:45 -0400

    cleaned up code for readability and clarity

--- recursion-and-dynamic-programming/NQueens.java
+++ recursion-and-dynamic-programming/NQueens.java
@@ -8,30 +8,30 @@ public class NQueens {
         ArrayList<ArrayList<String>> solutions = new ArrayList<ArrayList<String>>();
         ArrayList<String> board = generateBoard(a);
 
-        nQueens(0, board, a, solutions);
+        nQueensHelper(0, board, a, solutions);
 
         return solutions;
     }
 
-    public static void nQueens(int row, ArrayList<String> board, int queens, ArrayList<ArrayList<String>> solutions) {
+    public static void nQueensHelper(int row, ArrayList<String> board, int numberOfQueens, ArrayList<ArrayList<String>> solutions) {
         // base case, row == size of board
-        if (row == queens) {
+        if (row == numberOfQueens) {
             solutions.add(new ArrayList<String>(board));
         }
 
         // iterate over row
             // if next position for queen does not threaten others
                 // place queen, recur for next row
-        for (int col = 0; col < queens; col++) {
+        for (int col = 0; col < numberOfQueens; col++) {
             if (doesNotThreaten(board, row, col)) {
-                board.set(row, getRowString(col, queens));
-                nQueens(row + 1, new ArrayList<String>(board), queens, solutions);
+                placeQueen(board, row, col, numberOfQueens);
+                nQueensHelper(row + 1, new ArrayList<String>(board), numberOfQueens, solutions);
             }
         }
     }
 
     // where row and col are position of newly placed queen
-    // return true is newly placed queen threatens any others
+    // return true if newly placed queen does not threaten any other queens
     public static boolean doesNotThreaten(ArrayList<String> board, int row, int col) {
         for (int i = 0; i < row; i++) {
             int queenIndexInRow = getQueenIndexInRow(board.get(i));
@@ -40,9 +40,13 @@ public class NQueens {
             }
         }
 
-        // System.out.println(board);
-        // System.out.println("Placing at: " + row + ", " + col + " did not threaten.");
         return true;
+    }
+
+    public static void placeQueen(ArrayList<String> board, int rowIndex, int col, int numberOfQueens) {
+        String rowContents = getRowContents(col, numberOfQueens);
+        // wipes any previous queen placements on current row
+        board.set(rowIndex, rowContents);
     }
 
     public static int getQueenIndexInRow(String row) {
@@ -56,7 +60,7 @@ public class NQueens {
         return -1;
     }
 
-    public static String getRowString(int col, int length) {
+    public static String getRowContents(int col, int length) {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < length; i++) {
@@ -67,7 +71,6 @@ public class NQueens {
             }
         }
 
-        // System.out.println(sb.toString());
         return sb.toString();
     }
 
@@ -81,11 +84,30 @@ public class NQueens {
         return board;
     }
 
+    public static <T> void printBoards(ArrayList<ArrayList<T>> boards) {
+        if (boards.size() == 0) {
+            System.out.println("No solutions.");
+        }
+
+        for (int i = 0; i < boards.size(); i++) {
+            System.out.println("\nSolved Board: " + (i + 1));
+            ArrayList<T> board = boards.get(i);
+            for (T row : board) {
+                System.out.println(row);
+            }
+        }
+
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         System.out.println("For 2 Queens: ");
-        System.out.println(solveNQueens(2));
+        printBoards(solveNQueens(2));
 
-        System.out.println("For 4 Queens: ");
-        System.out.println(solveNQueens(4));
+        System.out.print("For 4 Queens: ");
+        printBoards(solveNQueens(4));
+
+        System.out.print("For 5 Queens: ");
+        printBoards(solveNQueens(5));
     }
 }
